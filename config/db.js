71e194a95d39d29089c0e9b51c7ddb4e643abffa
enrique1994mex <enrique.antonio1029@gmail.com>
2021-11-14 19:35:06 -0600

    Se cambio la variable de entorno a DB_MONGO

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config({path: 'variables.env'});
 
 const conectarDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_Mongo, {
+        await mongoose.connect(process.env.DB_MONGO, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
